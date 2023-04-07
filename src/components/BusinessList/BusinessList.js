8cfe84c06ade700af134cf3384f8c56c62019726
Kimberly Dijkmans <kim.14420@outlook.com>
2023-04-07 16:15:26 +0200

    implemented search bar func + dynamic results

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -6,12 +6,11 @@ class BusinessList extends React.Component {
     render() {
         return (
             <div className="BusinessList">
-                <Business /> 
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
+                {
+                    this.props.businesses.map((business, i) => {
+                        return <Business key={i} business={business}/>
+                    })
+                }
             </div>
         )
     }
