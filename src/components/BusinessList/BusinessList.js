48dc8650c6bb0dee67324b7269ab7a6383382023
Kimberly Dijkmans <kim.14420@outlook.com>
2023-04-09 10:49:10 +0200

    changed Business component key to business id

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -7,8 +7,8 @@ class BusinessList extends React.Component {
         return (
             <div className="BusinessList">
                 {
-                    this.props.businesses.map((business, i) => {
-                        return <Business key={i} business={business}/>
+                    this.props.businesses.map((business) => {
+                        return <Business key={business.id} business={business}/>
                     })
                 }
             </div>
