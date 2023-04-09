43fb1ebfe7ac9bb9601b8f9135d586a1db9091eb
Kimberly Dijkmans <kim.14420@outlook.com>
2023-04-09 11:11:05 +0200

    made addresses clickable to open Google Maps

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -11,14 +11,16 @@ class Business extends React.Component {
                 <h2>{this.props.business.name}</h2>
                 <div className="Business-information">
                     <div className="Business-address">
-                    <p>{this.props.business.address}</p>
-                    <p>{this.props.business.city}</p>
-                    <p>{this.props.business.state} {this.props.business.zipCode}</p>
+                        <a href={this.props.business.googleMapsLink} target="_blank">
+                            <p>{this.props.business.address}</p>
+                            <p>{this.props.business.city}</p>
+                            <p>{this.props.business.state} {this.props.business.zipCode}</p>
+                        </a>
                     </div>
                     <div className="Business-reviews">
-                    <h3>{this.props.business.category}</h3>
-                    <h3 className="rating">{this.props.business.rating} stars</h3>
-                    <p>{this.props.business.reviewCount} reviews</p>
+                        <h3>{this.props.business.category}</h3>
+                        <h3 className="rating">{this.props.business.rating} stars</h3>
+                        <p>{this.props.business.reviewCount} reviews</p>
                     </div>
                 </div>
             </div>
